a7233a11e4c08f07c5d714e90b87190d45b2b041
Leonie Ertl <ertl.leonie2004@gmail.com>
2025-07-14 15:33:51 +0200

    route.ts deleted. priviliges are working

--- kvs/src/app/layout.tsx
+++ kvs/src/app/layout.tsx
@@ -1,12 +1,9 @@
-'use client';
 import { Geist, Geist_Mono } from "next/font/google";
-import { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation'
-import { ToasterProvider } from "@/components/ui/toaster";
-import Sidebar from '@/components/navigation/Sidebar';
-import Navbar from '@/components/navigation/Navbar';
+import NavAndSidebar from "@/components/navigation/navAndSidebar";
+import { auth0 } from "@/lib/auth0";
 import "./globals.css";
 import '../styles/components.css';
+import { redirect } from "next/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,53 +15,30 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const [isOpen, setOpen] = useState(true);
-  const [user, setUser] = useState<string | null>(null);
-  const router = useRouter();
+  let user = undefined
 
-  useEffect(() => {
-    const checkAuth = async () => {
-      try {
-        const res = await fetch('/api/session');
-        const data = await res.json();
-        if (data.user) {
-          setUser(data.user.email);
-        } else {
-          setUser(null);
-          router.push('/auth/login')
-        }
-      } catch (error) {
-        console.error("Authentication check failed:", error);
-        setUser(null);
-        router.push('/auth/login')
-      }
-    };
-    checkAuth();
-  }, [router]);
+  const session = await auth0.getSession();
+  if (session && session.user) {
+    user = session.user.email; // Nur die E-Mail speichern
+  } else {
+    redirect('/auth/login');
+  }
+  if (user === undefined) {
+    redirect('/auth/login');
+  }
+
 
   return (
     <html lang="de">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-          <ToasterProvider>
-            {user == null ? (
-              <div className="min-h-screen flex flex-col">
-                {/* Hier könntest du einen Loader anzeigen */}
-              </div>
-            ) : (
-              <div className="min-h-screen flex flex-col">
-                <Navbar isOpen={isOpen} setOpen={setOpen} user={user} />
-                <div className="flex grow">
-                  <Sidebar isOpen={isOpen} />
-                  <main className="flex-1 transition-all duration-200">{children}</main>
-                </div>
-              </div>
-            )}
-          </ToasterProvider>
+        <NavAndSidebar user={user}>
+          {children}
+        </NavAndSidebar>
       </body>
     </html>
   );
---
+++ kvs/src/components/navigation/navAndSidebar.tsx
@@ -0,0 +1,29 @@
+'use client';
+import { useState } from 'react';
+
+import Navbar from '@/components/navigation/Navbar';
+import Sidebar from '@/components/navigation/Sidebar';
+import { ToasterProvider } from "@/components/ui/toaster";
+
+
+export default function NavAndSidebar({ user, children }: { user: string, children: React.ReactNode }) {
+  const [isOpen, setOpen] = useState(true);
+
+  return (
+    <ToasterProvider>
+      {user == null ? (
+        <div className="min-h-screen flex flex-col">
+          {/* Hier könntest du einen Loader anzeigen */}
+        </div>
+      ) : (
+        <div className="min-h-screen flex flex-col">
+          <Navbar isOpen={isOpen} setOpen={setOpen} user={user} />
+          <div className="flex grow">
+            <Sidebar isOpen={isOpen} />
+            <main className="flex-1 transition-all duration-200">{children}</main>
+          </div>
+        </div>
+      )}
+    </ToasterProvider>
+  );
+}
