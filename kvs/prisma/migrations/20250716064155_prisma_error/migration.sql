fa472402178f2ec33b48bad17a72d1204c929f41
Gyula Horvath <gylhrvth@gmail.com>
2025-07-16 12:07:23 +0200

    Ich atme prisma

--- kvs/prisma/migrations/20250716064155_prisma_error/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `discountAmount` on the `Invoice` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `Invoice` DROP COLUMN `discountAmount`,
-    ADD COLUMN `finalAmount` DECIMAL(10, 2) NULL;
