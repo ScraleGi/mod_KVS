861202629a61178e8e190b223872cb064af02f89
Leonie Ertl <ertl.leonie2004@gmail.com>
2025-07-18 10:36:22 +0200

    Refactor ProgramsPage to use TableTopButton component and improve code formatting

--- kvs/src/components/navigation/TableTopButton.tsx
+++ kvs/src/components/navigation/TableTopButton.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-export default function AuthTableTopButton({ title, button1, button2, button3, auth }: {
+export default function TableTopButton({ title, button1, button2, button3, auth }: {
     title: string
     button1: string,
     button2?: string,
@@ -16,8 +16,8 @@ export default function AuthTableTopButton({ title, button1, button2, button3, a
                     <Link
                         href={button1}
                         className="p-2 rounded-md bg-blue-300 text-white transition"
-                        title="Add New Area"
-                        aria-label="Add New Area"
+                        title="Add New"
+                        aria-label="Add New"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 20 20" stroke="currentColor">
                             <path fillRule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clipRule="evenodd" />
@@ -38,8 +38,8 @@ export default function AuthTableTopButton({ title, button1, button2, button3, a
                     <Link
                         href={button3}
                         className="p-2 rounded-md bg-red-100 text-gray-700 transition"
-                        title="Deleted Areas"
-                        aria-label="Deleted Areas"
+                        title="Deleted"
+                        aria-label="Deleted"
                     >
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 256 256" fill="currentColor">
                             <path d="M96,208a8,8,0,0,1-8,8H40a24,24,0,0,1-20.77-36l34.29-59.25L39.47,124.5A8,8,0,1,1,35.33,109l32.77-8.77a8,8,0,0,1,9.8,5.66l8.79,32.77A8,8,0,0,1,81,148.5a8.37,8.37,0,0,1-2.08.27,8,8,0,0,1-7.72-5.93l-3.8-14.15L33.11,188A8,8,0,0,0,40,200H88A8,8,0,0,1,96,208Zm140.73-28-23.14-40a8,8,0,0,0-13.84,8l23.14,40A8,8,0,0,1,216,200H147.31l10.34-10.34a8,8,0,0,0-11.31-11.32l-24,24a8,8,0,0,0,0,11.32l24,24a8,8,0,0,0,11.31-11.32L147.31,216H216a24,24,0,0,0,20.77-36ZM128,32a7.85,7.85,0,0,1,6.92,4l34.29,59.25-14.08-3.78A8,8,0,0,0,151,106.92l32.78,8.79a8.23,8.23,0,0,0,2.07.27,8,8,0,0,0,7.72-5.93l8.79-32.79a8,8,0,1,0-15.45-4.14l-3.8,14.17L148.77,28a24,24,0,0,0-41.54,0L84.07,68a8,8,0,0,0,13.85,8l23.16-40A7.85,7.85,0,0,1,128,32Z" />
