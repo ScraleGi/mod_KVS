7a398e2d4732d47acccc2e63e41ce349c2a895eb
Carlos Artiaga <carlesam89@gmail.com>
2025-07-23 11:53:31 +0200

    merge_update

--- kvs/src/components/navigation/Sidebar.tsx
+++ kvs/src/components/navigation/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { FaHome, FaCalendarAlt, FaCog, FaRegEnvelope, FaChartBar, FaThLarge, FaLayerGroup, FaUsers, FaChalkboardTeacher, FaUserShield, FaRegAddressCard, FaFileInvoiceDollar} from 'react-icons/fa';
+import { FaHome, FaCalendarAlt, FaCog, FaThLarge, FaLayerGroup, FaUsers, FaChalkboardTeacher, FaUserShield, FaRegAddressCard, FaFileInvoiceDollar} from 'react-icons/fa';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
